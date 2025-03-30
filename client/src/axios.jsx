aba2d2374d6c415492b31ca859a87792badf53f9
VeluruPavankumar <pavanchowdary85831708@gmail.com>
2025-03-30 19:40:48 +0530

    first commit

---
+++ client/src/axios.jsx
@@ -0,0 +1,22 @@
+import axios from "axios";
+
+const instance = axios.create({
+  baseURL: "https://spice-mania-be.onrender.com/api",
+  // baseURL: "/api",
+});
+
+// Add a request interceptor to include the authorization header
+instance.interceptors.request.use(
+  (config) => {
+    const authToken = localStorage.getItem("authToken");
+    if (authToken) {
+      config.headers.Authorization = authToken;
+    }
+    return config;
+  },
+  (error) => {
+    return Promise.reject(error);
+  }
+);
+
+export default instance;
