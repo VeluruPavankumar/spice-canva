8c341aba92836da71bc19406cefce534632c06fa
VeluruPavankumar <pavanchowdary85831708@gmail.com>
2025-04-03 23:00:34 +0530

    afsdfsd

--- client/src/axios.jsx
+++ client/src/axios.jsx
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const instance = axios.create({
-  baseURL: `${process.env.REACT_APP_API_BE_URL}/api`,
+  baseURL: `${import.meta.env.VITE_BE_URL}/api`,
   // baseURL: "/api",
 });
 
